f772c37b10b37ff2665e73687cc71aea4e08803d
carolnesso <carolnesso@gmail.com>
2025-10-21 11:42:16 -0300

    Exporta os dialogs

---
+++ lib/design_system/components/dialogs/wtech_dialogs.dart
@@ -0,0 +1,5 @@
+export 'wtech_dialog_base.dart';
+export 'wtech_loading_dialog.dart'
+    show showWtechLoading, showLoadingDialog, WtechLoadingDialog;
+export 'wtech_confirm_dialog.dart' show showWtechConfirm, WtechConfirmDialog;
+export 'wtech_change_password_dialog.dart' show WtechChangePasswordDialog;
